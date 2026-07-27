6eccc8cd4f8b637c0bb63b95d074e58187e0d6ff
FeihongChuSecE <faye.chu.fh@gmail.com>
2026-07-26 22:25:18 -0400

    user basic
    user register, login/logout, profile, getUserById

---
+++ src/main/java/com/example/petadoption/model/User.java
@@ -0,0 +1,67 @@
+package com.example.petadoption.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+}
