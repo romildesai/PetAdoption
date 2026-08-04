1df2c2f3063e977f9b753bb2c07b81a72ea0980c
NuntichaThummee <nunticha.ppalaloy909@gmail.com>
2026-08-04 02:46:34 -0400

    change path in securityConfig to match with adminController

--- src/main/java/com/example/petadoption/security/SecurityConfig.java
+++ src/main/java/com/example/petadoption/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
     public SecurityConfig(CustomUserDetailsService userDetailsService) {
@@ -30,7 +32,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/petAdoption/user/register", "/css/**").permitAll()
-                        .requestMatchers("/petAdoption/user/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/petAdoption/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
 // Hook your custom user service
