a709ddad98539e371112f94ce501a672dad490c9
avisinxh <159095688+avisinxh@users.noreply.github.com>
2026-08-09 18:21:18 -0400

    Update PetRestController.java

--- src/main/java/com/example/petadoption/rest/PetRestController.java
+++ src/main/java/com/example/petadoption/rest/PetRestController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/pets")
-public class PetRESTController {
+public class PetRestController {
 
     private final PetService petService;
 
